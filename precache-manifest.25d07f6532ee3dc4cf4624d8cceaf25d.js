df8a9cb7c21bd15f1cb43e51bb62c277b4d0ce34
Amit Srivastava <amit.srivastava4all@gmail.com>
2019-07-18 12:02:36 +0530

    Test

---
+++ precache-manifest.25d07f6532ee3dc4cf4624d8cceaf25d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56698aa5dc21bf790fa36bb0ac09011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a62c38417a782fb49b6",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "74c7ddd6f283b785e9e5",
+    "url": "/static/js/2.60193152.chunk.js"
+  },
+  {
+    "revision": "0a62c38417a782fb49b6",
+    "url": "/static/js/main.9981fb14.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
